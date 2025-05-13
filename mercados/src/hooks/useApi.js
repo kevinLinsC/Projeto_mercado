aac57031fcceaa4bb4c04ae5b123a97701c87841
kevinLinsC <kevin.l.andrade@aluno.senai.br>
2025-05-12 21:18:17 -0300

    Muito brabo mesmo

--- mercados/src/hooks/useApi.js
+++ mercados/src/hooks/useApi.js
@@ -1,44 +1,72 @@
 // Importa a url da api vindo do .env
 const url = import.meta.env.VITE_API_URL;
 
-import { useEffect, useState, useContext } from "react";
+import { useState, useEffect, useContext } from "react";
 
 import { AuthContext } from "../contexts/UserContext";
 
-export function useVerificaLogin(){
-    const [usuarios, setUsuarios] = useState([]);
+export function useVerificaLogin() {
+  const [usuarios, setUsuarios] = useState([]);
 
-    const { login } = useContext(AuthContext);
+  const { login } = useContext(AuthContext);
 
-    useEffect(() => {
-        async function fetchData() {
-            try{
-                const req = await fetch(`${url}/usuarios`);
-                const users = await req.json();
-                setUsuarios(users);
-            }   
-            catch(error){
-                console.log(error.message);
-            } 
-        }
-
-        fetchData();
-    }, [])
-
-    const verificaLogin = (data) => {
-        const userToFind = usuarios.find((user) => {
-            return user.email === data.email;
-        });
-
-        if(userToFind != undefined && userToFind.senha == data.senha) {
-            login(userToFind);
-            console.log("Usuário logado", userToFind.nome);
-            return "Login efetuado com sucesso";
-        }
-        else {
-            return "Usuário ou senha inválidos";
-        }
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const req = await fetch(`${url}/usuarios`);
+        const users = await req.json();
+        setUsuarios(users);
+      } catch (erro) {
+        console.log(erro.message);
+      }
     }
+    fetchData();
+  }, []);
 
-    return { verificaLogin };
+  const verificaLogin = (data) => {
+    const userToFind = usuarios.find((user) => {
+      return user.email === data.email;
+    });
+
+    if (userToFind != undefined && userToFind.senha == data.senha) {
+      login(userToFind);
+      console.log("Usuário logado", userToFind.nome);
+      return "Login efetuado com sucesso";
+    } else {
+      return "Usuário ou senha inválidos";
+    }
+  };
+  return { verificaLogin };
+}
+
+export function useListaProdutos() {
+  const [produtos, setProdutos] = useState([]);
+
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const req = await fetch(`${url}/produtos`);
+        const produtos = await req.json();
+        setProdutos(produtos);
+      } catch (erro) {
+        console.log(erro.message);
+      }
+    }
+    fetchData();
+  }, []);
+
+  return produtos
+}
+
+export function useDeleteProduto() {
+  const deletarProduto = async (idProduto) => {
+    const req = await fetch(`${url}/produtos/${idProduto}`, {
+      method:"DELETE"
+    } )
+    const res = await req.json();
+    console.log("Produto deletado:", res)
+    return res
+  }
+
+  return { deletarProduto}
 }
--- mercados/src/pages/Home.jsx
+++ mercados/src/pages/Home.jsx
@@ -1,8 +1,38 @@
 import React from 'react'
+import CardProduto from '../components/CardProduto'
+
+import { useListaProdutos } from '../hooks/useApi'
 
 const Home = () => {
+  const produtos = useListaProdutos();
+
   return (
-    <div>Home</div>
+    <div>
+      <h1>Lista</h1>
+      {/* Usar o flex-wrap para criar os cursos */}
+      <div className='d-flex col-12 gap-2 mt-3 justify-content-between flex-wrap'>
+        <CardProduto 
+          id='1'
+          nome='Shampoo pra homem'
+          descricao='Shampoo pra homem'
+          preco='9,99'
+          categoria='Saúde, limpeza, beleza e mecânico'
+          imagemUrl='https://preview.redd.it/3sz6drpuoal41.jpg?auto=webp&s=f4801bed96c11d40aa547e70ee9f24b75cfeddde'
+        />
+
+        {produtos.map((prod) => (
+          <CardProduto 
+            key={prod.id}
+            id={prod.id}
+            nome={prod.nome}
+            descricao={prod.descricao}
+            preco={prod.preco}
+            categoria={prod.categoria}
+            imagemUrl={prod.imagemUrl}
+          />
+        ))}
+      </div>
+    </div>
   )
 }
 
